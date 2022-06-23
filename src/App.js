58e5ce786950cddeefb3a8265736debd7d4a3df9
Johan Beskow <johan@beskow.org>
2022-06-24 00:16:23 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import './Styles/global.css'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Navbar />
   )
 }
 
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="navbar-wrapper">
+        <div className="navbar-container">
+            <ul>
+                <li>
+                    <a href="#">Hem</a>
+                </li>
+                <li>
+                    <a href="#">Öl</a>
+                </li>
+                <li>
+                    <a href="#">Whiskey</a>
+                </li>
+                <li>
+                    <a href="#">Kontakt</a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
